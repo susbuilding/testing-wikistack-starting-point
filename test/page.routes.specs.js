6a027701f859ec63eef726192904d6733d2884ec
Susanna Huang <dearsusannahuang@gmail.com>
2017-01-24 17:37:26 -0500

    get and posts done

--- test/page.routes.specs.js
+++ test/page.routes.specs.js
@@ -2,6 +2,7 @@ var supertest = require('supertest');
 var app = require('../app');
 var agent = supertest.agent(app);
 var Page = require('../models/index').Page;
+var expect = require('chai').expect;
 
 describe('http requests', function () {
     //beforeEach(function(){
@@ -34,12 +35,12 @@ describe('http requests', function () {
   describe('GET /wiki/:urlTitle', function () {
     it('responds with 404 on page that does not exist', function(done){
         agent
-        .get('/wiki/').params(urlTitle)
+        .get('/wiki/blehblah')
         .expect(404, done);
     });
     it('responds with 200 on page that does exist', function(done){
         agent
-        .get('/wiki/:urlTitle')
+        .get('/wiki/pagecreated')
         .expect(200, done);
     });
 
@@ -56,19 +57,43 @@ describe('http requests', function () {
   describe('GET /wiki/:urlTitle/similar', function () {
     it('responds with 404 for page that does not exist', function(done){
         agent
-        .get('/wiki/:urlTitle/similar')
+        .get('/wiki/urlTitle/similar')
         .expect(404, done);
     });
-    // it('responds with 200 for similar page', function(done){
-    //     agent
-    //     .get('/wiki/:urlTitle/similar')
-    //     .expect(200, done);
-    // });
+    it('responds with 200 for similar page', function(done){
+        agent
+        .get('/wiki/pagecreated/similar')
+        .expect(200, done);
+    });
   });
 
   describe('POST /wiki', function () {
-    it('responds with 302');
-    it('creates a page in the database');
+    it('responds with 302', function(done){
+        agent
+        .post('/wiki')
+        .send({
+            urlTitle: 'pagecreated'
+        })
+        expect(302);
+        done();
+    });
+    it('creates a page in the database', function(done){
+       var newPage = Page.create({
+              title: 'pagecreated2',
+              urlTitle:'pagecreated2',
+              content: 'content2',
+              status: 'open2',
+              tags: ['hi','bye']
+                });
+
+        agent
+        .post('/wiki')
+        .send({
+            newPage
+        })
+        expect(newPage).to.exist;
+        done();
+    });
   });
 
 });
